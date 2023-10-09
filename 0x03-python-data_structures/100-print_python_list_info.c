1f5e829b1f1a1f1e7f5d251cd4c383dcf54e18cc
achdaisy <oduordaizy@gmail.com>
2023-10-09 16:12:13 -0700

    slight changes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,25 @@
 #include <Python.h>
-/**
- * print_python_list_info - prints some basic info about python lists
- *
- * @p: a PyObject list
- * Return: nothing
- */
 
+/**
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
+ */
 void print_python_list_info(PyObject *p)
 {
+	int size, alloc, i;
 	PyObject *obj;
-	int i, size, buff;
 
 	size = Py_SIZE(p);
-	buff = ((PyListObject *)p)->allocated;
+	alloc = ((PyListObject *)p)->allocated;
 
 	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", buff);
+	printf("[*] Allocated = %d\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Element %d: ", i);
+
 		obj = PyList_GetItem(p, i);
-		printf("%s\n", Py_Type(obj)->tp_name);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,8 +1,10 @@
 #ifndef MY_HEADER_FILE
 #define MY_HEADER_FILE
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /*my protypes*/
-void print_python_list_info(PyObject *p);
 int is_palindrome(listint_t **head);
 
 #endif
