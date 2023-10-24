b016c8b1395101c871e7b23b27f33d2fb8b15f60
achdaisy <oduordaizy@gmail.com>
2023-10-23 17:31:57 -0700

    print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    no_of_elements = 0
+
+    for elements in range(x):
+        try:
+            print("{}".format(my_list[elements]), end="")
+            no_of_elements += 1
+        except IndexError:
+            break
+    print("")
+    return no_of_elements
