2311c7f95087ac817dfe00986f38960f06ff08c3
achdaisy <oduordaizy@gmail.com>
2023-10-30 17:27:11 -0700

    fixes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -62,4 +62,5 @@ class Rectangle:
         return "\n".join(['#' * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
+        """formal representation of the class"""
         return("Rectangle({},{})".format(self.__width, self.__height))
